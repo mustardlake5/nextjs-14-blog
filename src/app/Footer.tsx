cdc5989c065a128811112c7e49d12a18ff24ef76
mustardlake5 <mustardlake5@gmail.com>
2024-02-12 01:20:40 +0900

    ブログ共通フッタを作成

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t text-center py-3">
+      <span>@2024 mustardlake5</span>
+    </footer>
+  );
 };
 
 export default Footer;
