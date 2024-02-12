7b8450414771a53621048fba08fc36aeaf1cf093
mustardlake5 <mustardlake5@gmail.com>
2024-02-12 17:19:34 +0900

    ローディング機能を実装

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="grid place-items-center min-h-[70vh]">
+      <div className="w-16 h-16 border-t-4 border-orange-500 rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loading;
