5f69891d65fcf077ead18427e7ab5db1659f5c68
mustardlake5 <mustardlake5@gmail.com>
2024-02-12 18:29:48 +0900

    Articleに対するNotFoundページを作成

---
+++ src/app/articles/not-found.tsx
@@ -0,0 +1,12 @@
+const NotFound = () => {
+  return (
+    <div className="grid place-items-center min-h-[70vh]">
+      <div className="my-8 p-8 bg-white text-center rounded-lg">
+        <h1 className="text-2xl font-bold text-gray-800">404</h1>
+        <p className="mt-4 text-gray-600">ページが見つかりませんでした。</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
