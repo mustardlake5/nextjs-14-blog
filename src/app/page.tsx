7e5b321de2453d6df219fecc6ded6288b30a9aad
mustardlake5 <mustardlake5@gmail.com>
2024-02-12 11:33:29 +0900

    Homeページのデザインを微調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import ArticleList from "./components/ArticleList";
 
 export default function Home() {
   return (
-    <div className="md:flex justify-between">
+    <div className="md:flex justify-between mx-3 md:mx-5 my-5">
       <section className="w-full md:w-2/3">
         <ArticleList />
       </section>
 
-      <aside className="w-full md:w-1/3 flex flex-col items-center px-3 md:pl-6">
+      <aside className="w-full md:w-1/3 flex flex-col items-center md:pl-6">
         <div className="bg-white shadow-md rounded p-4 mb-6 mt-4">
           <h3 className="font-bold text-gray-900 mb-2">About Me</h3>
           <p className="text-gray-600">
