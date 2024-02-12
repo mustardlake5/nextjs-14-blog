50dc1a9c8ef513b64bdb21ee7992151190894b36
mustardlake5 <mustardlake5@gmail.com>
2024-02-12 18:20:44 +0900

    Articleを実装

---
+++ src/app/articles/[id]/page.tsx
@@ -0,0 +1,28 @@
+import { getArticleById } from "@/blogAPI";
+import Image from "next/image";
+
+const Article = async ({ params }: { params: { id: string } }) => {
+  const article = await getArticleById(params.id);
+
+  return (
+    <div className="max-w-3xl mx-auto m-5">
+      <Image
+        src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${article.id}`}
+        alt=""
+        width={1280}
+        height={300}
+      />
+      <div className="flex flex-col">
+        <span className="mt-4 text-sm text-right text-pink-400">
+          Published on {article.createdAt}
+        </span>
+        <h1 className="text-4xl text-center mt-8">{article.title}</h1>
+        <p className="mt-10 text-lg leading-relaxed text-justify">
+          {article.content}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Article;
--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,4 +1,5 @@
 import { Article } from "./types";
+import { notFound } from "next/navigation";
 
 export const getAllArticles = async (): Promise<Article[]> => {
   const response = await fetch("http://localhost:3001/articles", {
@@ -9,6 +10,23 @@ export const getAllArticles = async (): Promise<Article[]> => {
     throw new Error("エラーが発生しました");
   }
 
-  const articles = await response.json();
+  const articles: Article[] = await response.json();
   return articles;
+};
+
+export const getArticleById = async (id: string): Promise<Article> => {
+  const response = await fetch(`http://localhost:3001/articles/${id}`, {
+    next: { revalidate: 60 },
+  });
+
+  if (response.status === 404) {
+    notFound();
+  }
+
+  if (!response.ok) {
+    throw new Error("エラーが発生しました");
+  }
+
+  const article: Article = await response.json();
+  return article;
 };
